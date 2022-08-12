0b751131c0ee7c08e37ba5fa80dcc4d6d2d90484
username <doanducminh11082002@gmail.com>
2022-08-13 05:08:34 +0700

    Chua btvnB5

--- Buoi5_ChuaBtvn/src/Bai3/RunMain.java
+++ Buoi5_ChuaBtvn/src/Bai3/RunMain.java
@@ -2,34 +2,49 @@ package Bai3;
 
 public class RunMain {
     public static void main(String[] args) {
-        if(checkPrime(2l)){
-            System.out.println("La so nt");
-        }else{
-            System.out.println("Khong la so nguyen to");
-        }
+        System.out.println(checkPrime(2));
+        System.out.println(checkPrime(5L));
+        System.out.println(checkPrime(7.2f));
+        System.out.println(checkPrime(17.0d));
 
     }
 
-    public static boolean checkPrime(int n){
-        if(n<2) return false;
-        for(int i = 2;i*i<=n;i++){
-            if(n%i==0) return false;
+    public static boolean checkPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+
+    public static boolean checkPrime(long n) {
+        if (n < 2) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
         }
         return true;
     }
 
-
-    public static boolean checkPrime(long n){
-        if(n<2) return false;
-        for(int i = 2;i*i<=n;i++){
-            if(n%i==0) return false;
+    public static boolean checkPrime(float n) {
+        if (n == Math.ceil(n)) {
+            if (n < 2) return false;
+            for (int i = 2; i * i <= n; i++) {
+                if (n % i == 0) return false;
+            }
+            return true;
         }
-        return true;
-    }
-    public static boolean checkPrime(float n){
         return false;
     }
-    public static boolean checkPrime(double n){
+
+    public static boolean checkPrime(double n) {
+        if (n - (int) n == 0) {
+            if (n < 2) return false;
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
         return false;
     }
 }
